20707b2c75d04b0a20484b6356794cea5c1b739e
Fabian Duran <ingfabianavellaneda@gmail.com>
2022-02-18 08:39:26 -0500

    Se inicializa la construccion de la vista frases

--- src/pages/Frases.js
+++ src/pages/Frases.js
@@ -1,9 +1,13 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getData } from '../api/api';
 import { configAxios as axios } from '../api/axios';
 import _ from 'lodash';
+import { Grid } from '@mui/material';
+import { BoxFrase } from '../components/Frases/BoxFrase';
 
 function Frases() {
+  const [frases, setFrases] = useState([]);
+
   const estructurarData = async() => {
     const dataFrases = await getData('quotes');
     const listAgrupada = _.chain(dataFrases).groupBy('author').value();
@@ -42,15 +46,30 @@ function Frases() {
       });
       if (foto.length > 0) item.foto = foto[0].data[0].img;
     });
+    return dataEstructurada;
   };
 
-  useEffect(() => {
-    getProfilePersonaje();
+  const loadData = async() => {
+    const dataFrases = await getProfilePersonaje();
+    setFrases(dataFrases)
+  };
+
+  useEffect(async() => {
+    loadData();
   }, []);
 
   return (
-    <>
-    </>
+    <Grid
+      container
+      spacing={3}>
+      {
+        frases.map(frase => (
+          <BoxFrase 
+            key={frase.personaje} 
+            frase={frase} />
+        ))
+      }
+    </Grid>
   )
 }
 
